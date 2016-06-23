5cf83a190e54927f9375a40e898ad765137aae2e
martina4 <martinapappalardo23@gmail.com>
2016-06-23 19:35:25 +0200

    aggiunte funzioni in file_io

--- file_io.c
+++ file_io.c
@@ -1,7 +1,3 @@
-#include <stdint.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <time.h>
 #include "file_io.h"
 
 
@@ -12,13 +8,13 @@ void print_header(struct file_header* header){
     printf("Dictionary size: %d\n",header->dictionary_size);
     printf("Symbol size: %d\n",header->symbol_size);
     printf("File Name: %s\n",header->filename);
-    printf("File Size: %d\n",header->file_size);
+    printf("File Size: %d\n",(int)header->file_size);
     printf("Last Modification: %s",ctime(&header->last_modification_time));
     printf("Checksum: %d\n",header->checksum);
 
 }
 
-FILE* get_file(const char* filename, struct file_header* header,int dictionary_size) {
+void get_header(const char* filename, struct file_header* header,int dictionary_size) {
 
     struct stat file_stat;
     int ret;
@@ -33,17 +29,10 @@ FILE* get_file(const char* filename, struct file_header* header,int dictionary_s
     header->dictionary_size = dictionary_size;
     header->symbol_size = SYMBOL_SIZE;
     header->filename = filename;
+    header->filename_len = strlen(filename) + 1;
     header->file_size = file_stat.st_size;
     header->last_modification_time = file_stat.st_atim.tv_sec;
     header->checksum = 0;       //TODO Checksum
-
-    print_header(header);
-
-
-
-
-
-
 
 }
 /*
@@ -65,5 +54,146 @@ uint32_t crc32b(const char* message) {
     return ~crc;
 }*/
 
+//funzione che controlla che il file di input sia diverso da quello di output per non sovrascrivere
+void compare_filenames(const char* input_name, const char* output_name)
+{
+	int ret = strcmp(input_name, output_name);
 
+	if(ret == 0){
+		printf("error in specifing parameters\n");
+		exit(0);
+	}
+}
+
+//funzione che apre un file
+FILE* open_file(const char* filename, u_int mode)
+{
+	FILE* fp = NULL;
+	if(filename == NULL || filename[0] == '\0' || mode > 2){
+		printf("open_file: filename not valid\n");
+		return NULL;
+	}
+	switch(mode){
+		case READ:
+			fp = fopen(filename, "r");
+			break; 
+		case WRITE:
+			fp = fopen(filename, "w");
+			break; 
+		case APPEND:
+			fp = fopen(filename, "a");
+			break; 
+	}
+	if(fp == NULL){
+		printf("open_file: fopen() failed\n");
+		return NULL;
+	}
+	return fp;
+}
+
+void read_data(void* dest, int n, int size, FILE* fp)
+{
+	if (fread(dest, n, size, fp) < size){
+		exit(1);
+	}
+}
+
+void write_data(void* sr, int n, int size, FILE* fp)
+{
+	if(fwrite(sr, n, size, fp) < size){
+		exit(1);
+	}
+}
+
+void insert_header(const char* filename, int dictionary_size)
+{
+	FILE* fp;
+	struct file_header* head;
+	char* text;
+	int size = 0;
+	
+	head = (struct file_header*)malloc(sizeof(struct file_header));
+	get_header(filename, head, dictionary_size);
+	
+	size = head->file_size;
+	text = (char*)malloc(size + 1);
+
+	fp = open_file(filename, READ);
+	read_data(text, 1, head->file_size, fp);
+	text[size] = '\0';
+	fclose(fp);
+		
+	fp = open_file(filename, WRITE);
+
+	size = sizeof(int8_t);
+	write_data((void*)&(head->compression_algorithm_code), 1, size, fp);
+
+	fclose(fp);
+
+	fp = open_file(filename, APPEND);
+
+	size = sizeof(int32_t);
+	write_data((void*)&(head->dictionary_size), 1, size, fp);
+
+	size = sizeof(int32_t);
+	write_data((void*)&(head->symbol_size), 1, size, fp);
+	
+	size = sizeof(int32_t);
+	write_data((void*)&(head->filename_len), 1, size, fp);
+	
+	size = head->filename_len;
+	write_data((void*)(head->filename), 1, size, fp);
+
+	size = sizeof(off_t);
+	write_data((void*)&(head->file_size), 1, size, fp);
+
+	size = sizeof(time_t);
+	write_data((void*)&(head->last_modification_time), 1, size, fp);
+	
+	size = sizeof(int32_t);
+	write_data((void*)&(head->checksum), 1, size, fp);
+
+	size = strlen(text);
+	write_data((void*)(text), 1, size, fp);
+
+	fclose(fp);
+}
+
+void read_header(const char* filename)
+{
+	FILE* fp;
+	struct file_header* head;
+	int size;
+
+	head = (struct file_header*)malloc(sizeof(struct file_header));
+	
+	fp = open_file(filename, READ);
+	
+	size = sizeof(int8_t);
+	read_data((void*)&(head->compression_algorithm_code), 1, size, fp);
+
+	size = sizeof(int32_t);
+	read_data((void*)&(head->dictionary_size), 1, size, fp);
+
+	size = sizeof(int32_t);
+	read_data((void*)&(head->symbol_size), 1, size, fp);
+
+	size = sizeof(int32_t);
+	read_data((void*)&(head->filename_len), 1, size, fp);
+
+	size = head->filename_len;
+	head->filename = (char*)malloc(size);
+	read_data((void*)(head->filename), 1, size, fp);
+
+	size = sizeof(off_t);
+	read_data((void*)&(head->file_size), 1, size, fp);
+
+	size = sizeof(time_t);
+	read_data((void*)&(head->last_modification_time), 1, size, fp);
+	
+	size = sizeof(int32_t);
+	read_data((void*)&(head->checksum), 1, size, fp);
+	
+	//print_header(head);
+}
 
--- file_io.h
+++ file_io.h
@@ -1,12 +1,18 @@
 #define LZ_78_CODE 78
 #define SYMBOL_SIZE 8
 #define MAGIC_CODE 7878
+#define READ 0
+#define WRITE 1
+#define APPEND 2
 #ifndef STUDENT_LZ78_FILE_IO_H
 #define STUDENT_LZ78_FILE_IO_H
 
 #include <stdio.h>
-
-
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdint.h>
 
 
 struct file_header {
@@ -14,6 +20,7 @@ struct file_header {
     int8_t compression_algorithm_code;
     int32_t dictionary_size;
     int32_t symbol_size;    // The symbol size in bits (char ASCII = 8bit, Fax = 1 bit etc. )
+    int32_t filename_len;
     const char* filename;
     off_t file_size;
     time_t last_modification_time;
@@ -21,13 +28,16 @@ struct file_header {
 
 };
 
-FILE* get_file(const char*, struct file_header*,int);
+void get_header(const char*, struct file_header*,int);
 void print_header(struct file_header*);
+FILE* open_file(const char* filename, u_int mode);
+void compare_filenames(const char* input_name, const char* output_name);
+void read_data(void* dest, int n, int size, FILE* fp);
+void write_data(void* sr, int n, int size, FILE* fp);
+void insert_header(const char* filename, int dictionary_size);
+void read_header(const char* filename);
 // http://stackoverflow.com/questions/21001659/crc32-algorithm-implementation-in-c-without-a-look-up-table-and-with-a-public-li
 //uint32_t crc32b(const char *);
 
 
 #endif //STUDENT_LZ78_FILE_IO_H
-
-
-
